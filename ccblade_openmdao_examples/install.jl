9f3a6940199dd3ecddb1e364494aed373da5fa1f
kanekosh <shugok@umich.edu>
2022-06-16 17:46:12 -0400

    modified installation script

--- ccblade_openmdao_examples/install.jl
+++ ccblade_openmdao_examples/install.jl
@@ -1,9 +1,9 @@
 using Pkg
-Pkg.add("CCBlade")
+Pkg.develop(PackageSpec(path="/home/shugo/packages/CCBlade.jl"))   # developer install of the local package.
 Pkg.add("ConcreteStructs")
 Pkg.add("ForwardDiff")
 # OpenMDAO.jl isn't a Registered Julia Package®™©, so need to specify the url.
-Pkg.add(url="https://github.com/byuflowlab/OpenMDAO.jl.git")
+Pkg.add(PackageSpec(url="https://github.com/byuflowlab/OpenMDAO.jl"))
 Pkg.add("PyCall")
 Pkg.build("PyCall")
 using CCBlade
